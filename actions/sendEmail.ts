0cda52da03a74b99c4d8e36d5a8222471c3852b5
Webprojon <khasanovtokhir77@gmail.com>
2024-02-15 09:47:16 +0100

    updated

---
+++ actions/sendEmail.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import React from "react";
+import { getErrorMessage, validateString } from "@/lib/utils";
+import { Resend } from "resend";
+import ContactFormEmail from "@/email/contact-form-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formData: FormData) => {
+	const senderEmail = formData.get("senderEmail");
+	const message = formData.get("message");
+
+	// Simple Server-Side Validation
+	if (!validateString(senderEmail, 500)) {
+		return {
+			error: "Invalid sender email",
+		};
+	}
+
+	if (!validateString(message, 5000)) {
+		return {
+			error: "Invalid message",
+		};
+	}
+
+	let data;
+	try {
+		data = await resend.emails.send({
+			from: "Contact Form <onboarding@resend.dev>",
+			to: "tokhirjonkhasanov@gmail.com",
+			subject: "Message From Your Contact Form",
+			reply_to: senderEmail as string,
+			react: React.createElement(ContactFormEmail, {
+				message: message as string,
+				senderEmail: senderEmail as string,
+			}),
+		});
+	} catch (error: unknown) {
+		return {
+			error: getErrorMessage(error),
+		};
+	}
+
+	return {
+		data,
+	};
+};
