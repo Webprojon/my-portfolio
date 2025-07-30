6e9f38d6f18f1d23586f7cd8a1aff734bc68103c
Webprojon <tokhirjonkhasanov@gmail.com>
2025-07-30 22:34:12 +0200

    feat: this crazy stars added :))

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ const App = () => {
 				<Works />
 				<Feedbacks />
 				<div className="relative z-0">
-					{/*<Contact />*/}
-					{/*<StarsCanvas />*/}
+					<Contact />
+					<StarsCanvas />
 				</div>
 			</div>
 		</BrowserRouter>
